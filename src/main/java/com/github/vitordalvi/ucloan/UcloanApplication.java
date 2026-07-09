7b0f454470d2d29519ac392b03d547c3b6f987a5
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-09 18:28:47 -0300

    chore: initializing repository

---
+++ src/main/java/com/github/vitordalvi/ucloan/UcloanApplication.java
@@ -0,0 +1,13 @@
+package com.github.vitordalvi.ucloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcloanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcloanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/vitordalvi/ucloan/UcloanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vitordalvi.ucloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcloanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
