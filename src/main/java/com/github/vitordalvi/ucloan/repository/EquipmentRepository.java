8f47db09cd50d76ad30691c0176c1f3b64f882bb
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-11 21:07:50 -0300

    feat: created equipment repository

---
+++ src/main/java/com/github/vitordalvi/ucloan/repository/EquipmentRepository.java
@@ -0,0 +1,9 @@
+package com.github.vitordalvi.ucloan.repository;
+
+import com.github.vitordalvi.ucloan.entities.Equipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
+}
