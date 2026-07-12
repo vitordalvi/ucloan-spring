0753338aa11be1e2e802e50cbb57850c76522904
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-11 21:15:26 -0300

    feat: created loan history repository

---
+++ src/main/java/com/github/vitordalvi/ucloan/repository/LoanHistoryRepository.java
@@ -0,0 +1,9 @@
+package com.github.vitordalvi.ucloan.repository;
+
+import com.github.vitordalvi.ucloan.entities.LoanHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoanHistoryRepository extends JpaRepository<LoanHistory, Long> {
+}
