457820bfe3d7e32a956dd8e1c846019b97e13d05
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-13 20:49:57 -0300

    feat: added EquipmentModel request and response dtos

---
+++ src/main/java/com/github/vitordalvi/ucloan/dto/request/CreateEquipmentModelRequestDto.java
@@ -0,0 +1,3 @@
+package com.github.vitordalvi.ucloan.dto.request;
+
+public record CreateEquipmentModelRequestDto(String name, String manufacturer) {}
---
+++ src/main/java/com/github/vitordalvi/ucloan/dto/response/EquipmentModelResponseDto.java
@@ -0,0 +1,3 @@
+package com.github.vitordalvi.ucloan.dto.response;
+
+public record EquipmentModelResponseDto(Long id, String name, String manufacturer) {}
