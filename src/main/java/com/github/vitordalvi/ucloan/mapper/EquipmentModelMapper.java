a0d6bfc520b1d9274e5c543b365ad3769af97303
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-13 20:50:31 -0300

    feat: added EquipmentModel mapper interface

---
+++ src/main/java/com/github/vitordalvi/ucloan/mapper/EquipmentModelMapper.java
@@ -0,0 +1,21 @@
+package com.github.vitordalvi.ucloan.mapper;
+
+import com.github.vitordalvi.ucloan.dto.request.CreateEquipmentModelRequestDto;
+import com.github.vitordalvi.ucloan.dto.request.CreateEquipmentRequestDto;
+import com.github.vitordalvi.ucloan.dto.response.EquipmentModelResponseDto;
+import com.github.vitordalvi.ucloan.entities.EquipmentModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface EquipmentModelMapper {
+
+    EquipmentModel toEntity(CreateEquipmentModelRequestDto dto);
+    EquipmentModelResponseDto toDto(EquipmentModel equipmentModel);
+
+    List<EquipmentModelResponseDto> toDtoList(List<EquipmentModel> equipmentModels);
+
+    void updateEntityFromDto(CreateEquipmentModelRequestDto dto, @MappingTarget EquipmentModel equipmentModel);
+}
