e23a55bb8a8203c9797cf39e9282faf649e8a486
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-13 22:31:44 -0300

    feat: added dto for the patch endpoint

---
+++ src/main/java/com/github/vitordalvi/ucloan/dto/request/PatchEquipmentRequestDto.java
@@ -0,0 +1,12 @@
+package com.github.vitordalvi.ucloan.dto.request;
+
+import com.github.vitordalvi.ucloan.entities.enums.LoanStatus;
+import com.github.vitordalvi.ucloan.entities.enums.PhysicalStatus;
+import jakarta.validation.constraints.Size;
+
+public record PatchEquipmentRequestDto(
+        @Size(max = 500) String description,
+        Long equipmentModelId,
+        LoanStatus loanStatus,
+        PhysicalStatus physicalStatus
+) {}
