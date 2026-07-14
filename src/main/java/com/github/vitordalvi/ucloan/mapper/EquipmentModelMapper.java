4019c5c7c65b1bc28ebe87dc19f8b625f4f7121f
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-14 20:09:29 -0300

    feat: added new patch entity mapper

--- src/main/java/com/github/vitordalvi/ucloan/mapper/EquipmentModelMapper.java
+++ src/main/java/com/github/vitordalvi/ucloan/mapper/EquipmentModelMapper.java
@@ -2,6 +2,7 @@ package com.github.vitordalvi.ucloan.mapper;
 
 import com.github.vitordalvi.ucloan.dto.request.CreateEquipmentModelRequestDto;
 import com.github.vitordalvi.ucloan.dto.request.CreateEquipmentRequestDto;
+import com.github.vitordalvi.ucloan.dto.request.PatchEquipmentModelRequestDto;
 import com.github.vitordalvi.ucloan.dto.response.EquipmentModelResponseDto;
 import com.github.vitordalvi.ucloan.entities.EquipmentModel;
 import org.mapstruct.Mapper;
@@ -17,5 +18,6 @@ public interface EquipmentModelMapper {
 
     List<EquipmentModelResponseDto> toDtoList(List<EquipmentModel> equipmentModels);
 
+    void patchEntityFromDto(PatchEquipmentModelRequestDto dto, @MappingTarget EquipmentModel equipmentModel);
     void updateEntityFromDto(CreateEquipmentModelRequestDto dto, @MappingTarget EquipmentModel equipmentModel);
 }
