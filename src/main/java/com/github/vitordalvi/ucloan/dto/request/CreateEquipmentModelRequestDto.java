8027eb72eba8405aca16d1fa84eb9ea3534523d1
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-13 22:32:35 -0300

    feat: add validation annotations into dtos

--- src/main/java/com/github/vitordalvi/ucloan/dto/request/CreateEquipmentModelRequestDto.java
+++ src/main/java/com/github/vitordalvi/ucloan/dto/request/CreateEquipmentModelRequestDto.java
@@ -1,3 +1,8 @@
 package com.github.vitordalvi.ucloan.dto.request;
 
-public record CreateEquipmentModelRequestDto(String name, String manufacturer) {}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateEquipmentModelRequestDto(
+        @NotBlank @Size(min = 3, max = 120) String name,
+        @NotBlank @Size(min = 3, max = 120) String manufacturer) {}
--- src/main/java/com/github/vitordalvi/ucloan/dto/request/CreateEquipmentRequestDto.java
+++ src/main/java/com/github/vitordalvi/ucloan/dto/request/CreateEquipmentRequestDto.java
@@ -2,6 +2,11 @@ package com.github.vitordalvi.ucloan.dto.request;
 
 import com.github.vitordalvi.ucloan.entities.enums.LoanStatus;
 import com.github.vitordalvi.ucloan.entities.enums.PhysicalStatus;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-public record CreateEquipmentRequestDto(String description, Long equipmentModelId, LoanStatus loanStatus,
-                                        PhysicalStatus physicalStatus) {}
+public record CreateEquipmentRequestDto(
+        @Size(max = 500) String description,
+        @NotNull Long equipmentModelId,
+        @NotNull LoanStatus loanStatus,
+        @NotNull PhysicalStatus physicalStatus) {}
