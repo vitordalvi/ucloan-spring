f1eb3c6aecf6f5eb00e1b7b647d7d3c02cadb261
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-14 22:36:47 -0300

    feat: added specific query to find if equipment exists by it id and loan status

--- src/main/java/com/github/vitordalvi/ucloan/repository/LoanRepository.java
+++ src/main/java/com/github/vitordalvi/ucloan/repository/LoanRepository.java
@@ -1,9 +1,13 @@
 package com.github.vitordalvi.ucloan.repository;
 
 import com.github.vitordalvi.ucloan.entities.Loan;
+import com.github.vitordalvi.ucloan.entities.enums.LoanStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+    boolean existsByEquipmentIdAndLoanStatus(Long equipmentId, LoanStatus loanStatus);
 }
