c5dd32ea4e91245a2f53740b403f79b18035d357
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-16 20:12:53 -0300

    fix: white list url correction

--- src/main/java/com/github/vitordalvi/ucloan/config/SecurityConfig.java
+++ src/main/java/com/github/vitordalvi/ucloan/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
 
     private static final String[] WHITE_LIST_URL = {
             "/api/v1/auth/**",
-            "/v3/api-docs",
+            "/v3/api-docs/**",
             "/swagger-ui/**",
             "/swagger-ui.html"
     };
