fe7fdb3edb7c9ec6ea7fa5da4b67bcccced2babf
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-15 23:06:02 -0300

    feat: added new application user attributes

--- src/main/java/com/github/vitordalvi/ucloan/entities/ApplicationUser.java
+++ src/main/java/com/github/vitordalvi/ucloan/entities/ApplicationUser.java
@@ -1,26 +1,52 @@
 package com.github.vitordalvi.ucloan.entities;
 
 import com.github.vitordalvi.ucloan.entities.base.AuditableBaseEntity;
+import com.github.vitordalvi.ucloan.entities.enums.Role;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
-public class ApplicationUser extends AuditableBaseEntity {
+public class ApplicationUser extends AuditableBaseEntity implements UserDetails {
 
-    private String name;
+    private String firstName;
+    private String lastName;
     private String email;
+    private String password;
 
-    /* Depois quando implementar o Spring Security, vou
-        adicionar função de autenticação (senha), autoridade
-        (roles e permissions)e  JWT
-    */
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
-    public String getName() {
-        return name;
+    @OneToMany(mappedBy = "applicationUser")
+    private List<Token> tokens;
+
+    private boolean enabled = true;
+    private boolean accountNonExpired = true;
+    private boolean accountNonLocked = true;
+    private boolean credentialsNonExpired = true;
+
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
     }
 
     public String getEmail() {
@@ -29,5 +55,127 @@ public class ApplicationUser extends AuditableBaseEntity {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<Token> getTokens() {
+        return tokens;
+    }
+
+    public void addToken(Token token) {
+        tokens.add(token);
+        token.setUser(this);
+    }
+
+    // auth / security
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getAuthorities();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    public void setAccountNonExpired(boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String password;
+        private Role role;
+
+        public Builder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder role(Role role) {
+            this.role = role;
+            return this;
+        }
+
+        public ApplicationUser build() {
+            ApplicationUser user = new ApplicationUser();
+            user.setFirstName(this.firstName);
+            user.setLastName(this.lastName);
+            user.setEmail(this.email);
+            user.setPassword(this.password);
+            user.setRole(this.role);
+
+            return user;
+        }
     }
 }
--- src/main/java/com/github/vitordalvi/ucloan/entities/base/AuditableBaseEntity.java
+++ src/main/java/com/github/vitordalvi/ucloan/entities/base/AuditableBaseEntity.java
@@ -1,16 +1,32 @@
 package com.github.vitordalvi.ucloan.entities.base;
 
+import com.github.vitordalvi.ucloan.entities.ApplicationUser;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MappedSuperclass;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class AuditableBaseEntity extends BaseEntity {
-    @CreationTimestamp
+
+    @CreatedDate
     private LocalDateTime createdAt;
 
-    @UpdateTimestamp
+    @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    @CreatedBy
+    @ManyToOne
+    private ApplicationUser createdBy;
+
+    @LastModifiedBy
+    @ManyToOne
+    private ApplicationUser updatedBy;
 }
