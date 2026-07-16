553fd5db5f44662eaadf897d26398616f309050e
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-15 23:05:22 -0300

    feat: add user auth related queries

--- src/main/java/com/github/vitordalvi/ucloan/repository/ApplicationUserRepository.java
+++ src/main/java/com/github/vitordalvi/ucloan/repository/ApplicationUserRepository.java
@@ -4,6 +4,10 @@ import com.github.vitordalvi.ucloan.entities.ApplicationUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
+
+    Optional<ApplicationUser> findByEmail(String email);
 }
---
+++ src/main/java/com/github/vitordalvi/ucloan/repository/TokenRepository.java
@@ -0,0 +1,22 @@
+package com.github.vitordalvi.ucloan.repository;
+
+import com.github.vitordalvi.ucloan.entities.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Long> {
+
+    @Query(value = """
+        select t from Token t inner join ApplicationUser u\s
+                on t.applicationUser.id = u.id\s
+                where u.id = :id and (t.expired = false or t.revoked = false)\s
+        """)
+    List<Token> findAllValidTokenByUser(Long id);
+
+    Optional<Token> findByToken(String token);
+}
