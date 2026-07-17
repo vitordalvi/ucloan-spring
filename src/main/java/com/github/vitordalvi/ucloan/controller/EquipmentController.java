6917a123def8727e6eec6ed178a1a2958a16629a
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-16 21:32:45 -0300

    feat: added equipment history endpoint

--- src/main/java/com/github/vitordalvi/ucloan/controller/EquipmentController.java
+++ src/main/java/com/github/vitordalvi/ucloan/controller/EquipmentController.java
@@ -2,26 +2,32 @@ package com.github.vitordalvi.ucloan.controller;
 
 import com.github.vitordalvi.ucloan.dto.request.CreateEquipmentRequestDto;
 import com.github.vitordalvi.ucloan.dto.request.PatchEquipmentRequestDto;
+import com.github.vitordalvi.ucloan.dto.response.EquipmentHistoryResponseDto;
 import com.github.vitordalvi.ucloan.dto.response.EquipmentResponseDto;
+import com.github.vitordalvi.ucloan.services.EquipmentHistoryService;
 import com.github.vitordalvi.ucloan.services.EquipmentService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/equipments")
 public class EquipmentController {
 
     private final EquipmentService equipmentService;
+    private final EquipmentHistoryService equipmentHistoryService;
 
-    public EquipmentController(EquipmentService equipmentService) {
+    public EquipmentController(EquipmentService equipmentService, EquipmentHistoryService equipmentHistoryService) {
         this.equipmentService = equipmentService;
+        this.equipmentHistoryService = equipmentHistoryService;
     }
 
     @GetMapping("/{id}")
@@ -62,6 +68,7 @@ public class EquipmentController {
         return ResponseEntity.ok(response);
     }
 
+    // refazer (aplicar a logica de um "soft delete")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
 
@@ -74,5 +81,17 @@ public class EquipmentController {
         Page<EquipmentResponseDto> response = equipmentService.findAll(pageable);
 
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}/history")
+    public ResponseEntity<List<EquipmentHistoryResponseDto>> getHistory(@PathVariable Long id) {
+        List<EquipmentHistoryResponseDto> history = equipmentHistoryService.findAllByEquipmentId(id);
+        return ResponseEntity.ok(history);
+    }
+
+    @PreAuthorize("hasAuthority('admin:read')")
+    @GetMapping("/admin")
+    public String testAdminViewing() {
+        return "WORKING";
     }
 }
