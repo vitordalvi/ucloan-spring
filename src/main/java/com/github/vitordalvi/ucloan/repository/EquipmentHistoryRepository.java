69ab695d13e4998c256ef4d26adf33ae59fc7d1b
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-16 21:36:04 -0300

    feat: added new query returning a list of equipment history by it equipment id

--- src/main/java/com/github/vitordalvi/ucloan/repository/EquipmentHistoryRepository.java
+++ src/main/java/com/github/vitordalvi/ucloan/repository/EquipmentHistoryRepository.java
@@ -4,6 +4,9 @@ import com.github.vitordalvi.ucloan.entities.EquipmentHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EquipmentHistoryRepository extends JpaRepository<EquipmentHistory, Long> {
+    List<EquipmentHistory> findAllByEquipmentId(Long equipmentId);
 }
