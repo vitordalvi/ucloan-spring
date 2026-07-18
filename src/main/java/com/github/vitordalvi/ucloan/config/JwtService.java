706688e495c419bc0993fc7d88ff46aa1438cdb1
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-17 21:37:10 -0300

    docs: added jwt service features explained

--- src/main/java/com/github/vitordalvi/ucloan/config/JwtService.java
+++ src/main/java/com/github/vitordalvi/ucloan/config/JwtService.java
@@ -28,19 +28,23 @@ public class JwtService {
     @Value("${application.security.jwt.refresh-token.expiration}")
     private long refreshExpiration;
 
+    // Função para extrair o username do usuário pelo token
     public String extractUsername(String token) {
-        return extractClaim(token, Claims::getSubject);
+        return extractClaim(token, Claims::getSubject); // Acessa as claims do usuário
     }
 
+    // Extrai uma claim específica
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
+    // Função para retornar o token já criado
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
     }
 
+    // Função para criar o token
     public String generateToken(
             Map<String, Object> extraClaims,
             UserDetails userDetails
@@ -54,43 +58,49 @@ public class JwtService {
         return buildToken(new HashMap<>(), userDetails, refreshExpiration);
     }
 
+    // Construção do token
     private String buildToken(
-            Map<String, Object> extraClaims,
-            UserDetails userDetails,
-            long expiration
+            Map<String, Object> extraClaims, // Claims extras do token
+            UserDetails userDetails, // userDetails para conseguirmos adicionar o username (email) do usuário no token
+            long expiration // Tempo de expiração do token
     ) {
         return Jwts
-                .builder()
-                .claims(extraClaims)
-                .subject(userDetails.getUsername())
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(getSignInKey())
-                .compact();
+                .builder() // Inicia a configuração do token
+                .claims(extraClaims) // Adiciona as claims no token
+                .subject(userDetails.getUsername()) // Adiciona o username do usuário (que tamo usando o email)
+                .issuedAt(new Date(System.currentTimeMillis())) // Adiciona o momento em que o token foi emitido
+                .expiration(new Date(System.currentTimeMillis() + expiration)) // Adiciona o tempo de expiração
+                .signWith(getSignInKey()) // Assina o token com a key criptografada (para verificar se o token é valido depois)
+                .compact(); // Serializa o token em string
     }
 
+    // Retorna se o token é valido, pelo nome do usuário e regra de expiração
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
+    // Retorna se o token está ou não expirado, vendo se a expiração já passou
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
+    // Retorna a Expiration do Token pela claim
     private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
+    // Extrai todos os dados enviados no token
     private Claims extractAllClaims(String token) {
         return Jwts
-                .parser()
-                .verifyWith(getSignInKey())
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
+                .parser() // Inicia a configuração do parser de tokens
+                .verifyWith(getSignInKey()) // Valida o token com a key criptografada do sistema
+                .build() // Finaliza a configuração e monta o parser
+                .parseSignedClaims(token) // Parsing e verifica a assinatura, retornando as claims
+                .getPayload(); // Retorna a payload do token
     }
 
+    // Decodifica a key do sistema para validações
     private SecretKey getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
