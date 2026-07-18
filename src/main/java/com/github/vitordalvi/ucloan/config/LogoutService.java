a7bc53c077e734eb865613bf740a70d63437bf47
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-17 21:44:34 -0300

    docs: added logout logic and possible changes

--- src/main/java/com/github/vitordalvi/ucloan/config/LogoutService.java
+++ src/main/java/com/github/vitordalvi/ucloan/config/LogoutService.java
@@ -17,28 +17,43 @@ public class LogoutService implements LogoutHandler {
         this.tokenRepository = tokenRepository;
     }
 
+    /* Função de logout (vai usado como configuração de logout padrão do SecurityConfig)
+
+    Acho que essa aplicação da autenticação é boa pro contexto aprender e afins,
+    mas como eu coloquei na classe do AuthFilter, tornar o JWT Stateful
+    (Guardar o estado dele no banco e etc) pode não ser muito escalável e perde
+    a funcionalidade propriedade "Stateless" do JWT, mas pelo menos garante
+    que se um usuário realmente fizer login, esse ele não poderá logar com o mesmo
+    token, assim não podendo "falsificar" o login
+    */
     @Override
     public void logout(
             HttpServletRequest request,
             HttpServletResponse response,
             Authentication authentication
     ) {
+        // Armazena o value de Authorization do header
         final String authHeader = request.getHeader("Authorization");
-        final String jwt;
+        final String jwt; // Armazena o token jwt
 
+        // Se o authHeader for nulo ou não começar com "Bearer ", termina a função
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             return;
         }
 
+        // Corta a string até o 7o digito (onde começa o token)
         jwt = authHeader.substring(7);
-        var storedToken = tokenRepository.findByToken(jwt)
-                .orElse(null);
 
+        // Procura o token no banco, se achar, armazena o token na variável
+        var storedToken = tokenRepository.findByToken(jwt)
+                .orElse(null); // Se não achar, retorna como nulo
+
+        // Se achou o token
         if (storedToken != null) {
-            storedToken.setExpired(true);
-            storedToken.setRevoked(true);
-            tokenRepository.save(storedToken);
-            SecurityContextHolder.clearContext();
+            storedToken.setExpired(true); // Seta o token como expirado
+            storedToken.setRevoked(true); // Seta o token como revogado
+            tokenRepository.save(storedToken); // Salva o token
+            SecurityContextHolder.clearContext(); // Limpa a autenticação do usuário
         }
     }
 }
