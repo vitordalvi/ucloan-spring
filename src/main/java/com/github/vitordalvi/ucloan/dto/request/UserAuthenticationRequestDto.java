b742bfce51769609582088f93a7e88c4b4a2c679
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-17 22:54:14 -0300

    feat: added validation to email attribute dto

--- src/main/java/com/github/vitordalvi/ucloan/dto/request/UserAuthenticationRequestDto.java
+++ src/main/java/com/github/vitordalvi/ucloan/dto/request/UserAuthenticationRequestDto.java
@@ -1,3 +1,8 @@
 package com.github.vitordalvi.ucloan.dto.request;
 
-public record UserAuthenticationRequestDto(String email, String password) {}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserAuthenticationRequestDto(
+        @NotBlank @Email String email,
+        @NotBlank String password) {}
