1b5ea8053e6de2aeccf4857a9dff1d70a65df66d
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-17 23:47:24 -0300

    feat: added new business exception handler

--- src/main/java/com/github/vitordalvi/ucloan/exceptions/handler/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/github/vitordalvi/ucloan/exceptions/handler/CustomResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.github.vitordalvi.ucloan.exceptions.handler;
 
+import com.github.vitordalvi.ucloan.exceptions.BusinessException;
 import com.github.vitordalvi.ucloan.exceptions.ExceptionResponse;
 import com.github.vitordalvi.ucloan.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -18,19 +19,29 @@ import java.util.Date;
 @RestController
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
+    // Interceptador de todas as exceptions
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
         ExceptionResponse response = new ExceptionResponse(new Date(),
                 ex.getMessage(), request.getDescription(false));
 
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR); // Retorna 500 (Erro interno)
     }
 
+    // Implementação da exception customizada ResourceNotFound
     @ExceptionHandler(ResourceNotFoundException.class)
     public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception ex, WebRequest request) {
         ExceptionResponse response = new ExceptionResponse(new Date(),
                 ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public final ResponseEntity<ExceptionResponse> handleBusinessExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(new Date(),
+                ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
