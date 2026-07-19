62606e444fab02705c8be2fec42a590e47c8547d
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-19 20:22:45 -0300

    fix: removed some wrong mapper methods and add admin mappers

--- src/main/java/com/github/vitordalvi/ucloan/mapper/ApplicationUserMapper.java
+++ src/main/java/com/github/vitordalvi/ucloan/mapper/ApplicationUserMapper.java
@@ -1,21 +1,40 @@
 package com.github.vitordalvi.ucloan.mapper;
 
+import com.github.vitordalvi.ucloan.dto.request.UserAdminPatchRequestDto;
 import com.github.vitordalvi.ucloan.dto.request.UserPatchRequestDto;
 import com.github.vitordalvi.ucloan.dto.request.UserRegisterRequestDto;
+import com.github.vitordalvi.ucloan.dto.response.UserAdminResponseDto;
+import com.github.vitordalvi.ucloan.dto.response.UserResponseDto;
 import com.github.vitordalvi.ucloan.entities.ApplicationUser;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ApplicationUserMapper {
 
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "tokens", ignore = true)
+    @Mapping(target = "accountNonExpired", ignore = true)
+    @Mapping(target = "accountNonLocked", ignore = true)
+    @Mapping(target = "credentialsNonExpired", ignore = true)
     ApplicationUser toEntity(UserRegisterRequestDto dto);
-    UserRegisterRequestDto toDto(ApplicationUser user);
 
-    List<ApplicationUser> toDtoList(List<UserRegisterRequestDto> dto);
+    @Mapping(target = "createdAt", source = "createdAt")
+    @Mapping(target = "updatedAt", source = "updatedAt")
+    @Mapping(target = "updatedBy", source = "updatedBy.id")
+    @Mapping(target = "createdBy", source = "createdBy.id")
+    UserAdminResponseDto toAdminDto(ApplicationUser user);
+    UserResponseDto toDto(ApplicationUser user);
 
+    List<UserAdminResponseDto> toAdminDtoList(List<ApplicationUser> user);
+    List<UserResponseDto> toDtoList(List<ApplicationUser> user);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void patchEntityFromDto(UserPatchRequestDto dto, @MappingTarget ApplicationUser user);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void patchEntityFromAdminDto(UserAdminPatchRequestDto dto, @MappingTarget ApplicationUser user);
+
     void updateEntityFromDto(UserRegisterRequestDto dto, @MappingTarget ApplicationUser user);
 }
