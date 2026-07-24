c4b1c93e3e34fa468a82b2af1d5b02d544c1c195
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-23 21:42:35 -0300

    feat: added custom queries to appropriate endpoints

--- src/main/java/com/github/vitordalvi/ucloan/repository/LoanHistoryRepository.java
+++ src/main/java/com/github/vitordalvi/ucloan/repository/LoanHistoryRepository.java
@@ -1,9 +1,12 @@
 package com.github.vitordalvi.ucloan.repository;
 
 import com.github.vitordalvi.ucloan.entities.LoanHistory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoanHistoryRepository extends JpaRepository<LoanHistory, Long> {
+    Page<LoanHistory> findAllByLoanId(Long id, Pageable pageable);
 }
--- src/main/java/com/github/vitordalvi/ucloan/repository/LoanRepository.java
+++ src/main/java/com/github/vitordalvi/ucloan/repository/LoanRepository.java
@@ -2,12 +2,17 @@ package com.github.vitordalvi.ucloan.repository;
 
 import com.github.vitordalvi.ucloan.entities.Loan;
 import com.github.vitordalvi.ucloan.entities.enums.LoanStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+    boolean existsByEquipmentId(Long equipmentId);
     boolean existsByEquipmentIdAndLoanStatus(Long equipmentId, LoanStatus loanStatus);
+    Page<Loan> findAllByBorrowerId(Long borrowerId, Pageable pageable);
 }
