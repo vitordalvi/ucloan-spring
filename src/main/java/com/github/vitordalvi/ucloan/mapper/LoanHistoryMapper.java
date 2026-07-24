35bbb7fbcc8ce08e71b9a69c2ae680bef78b84d4
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-23 21:42:19 -0300

    feat: added mapper for endpoints

--- src/main/java/com/github/vitordalvi/ucloan/mapper/LoanHistoryMapper.java
+++ src/main/java/com/github/vitordalvi/ucloan/mapper/LoanHistoryMapper.java
@@ -1,7 +1,14 @@
 package com.github.vitordalvi.ucloan.mapper;
 
+import com.github.vitordalvi.ucloan.dto.response.LoanHistoryResponseDto;
+import com.github.vitordalvi.ucloan.entities.LoanHistory;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface LoanHistoryMapper {
+
+    @Mapping(target = "loanId", source = "loan.id")
+    @Mapping(target = "changedById", source = "changedBy.id")
+    LoanHistoryResponseDto toDto(LoanHistory loanHistory);
 }
--- src/main/java/com/github/vitordalvi/ucloan/mapper/LoanMapper.java
+++ src/main/java/com/github/vitordalvi/ucloan/mapper/LoanMapper.java
@@ -1,10 +1,13 @@
 package com.github.vitordalvi.ucloan.mapper;
 
 import com.github.vitordalvi.ucloan.dto.request.CreateLoanRequestDto;
+import com.github.vitordalvi.ucloan.dto.request.LoanPatchRequestDto;
+import com.github.vitordalvi.ucloan.dto.response.LoanAdminResponseDto;
 import com.github.vitordalvi.ucloan.dto.response.LoanResponseDto;
 import com.github.vitordalvi.ucloan.entities.Loan;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface LoanMapper {
@@ -13,7 +16,21 @@ public interface LoanMapper {
     @Mapping(target = "startDate", ignore = true)
     Loan toEntity(CreateLoanRequestDto dto);
 
-    @Mapping(target = "borrower", source = "borrower.Id")
-    @Mapping(target = "equipment", source = "equipment.Id")
+    @Mapping(target = "borrowerId", source = "borrower.id")
+    @Mapping(target = "equipmentId", source = "equipment.id")
+    @Mapping(target = "createdAt", source = "createdAt")
+    @Mapping(target = "updatedAt", source = "updatedAt")
+    @Mapping(target = "updatedBy", source = "updatedBy.id")
+    @Mapping(target = "createdBy", source = "createdBy.id")
+    LoanAdminResponseDto toAdminDto(Loan loan);
+
+    @Mapping(target = "borrowerId", source = "borrower.id")
+    @Mapping(target = "equipmentId", source = "equipment.id")
     LoanResponseDto toDto(Loan loan);
+
+    List<LoanResponseDto> toDtoList(List<Loan> loan);
+    List<LoanAdminResponseDto> toAdminDtoList(List<Loan> loan);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void patchEntityFromDto(LoanPatchRequestDto dto, @MappingTarget Loan loan);
 }
