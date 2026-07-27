f5807045a5965874752dff59f798e8a3a59a03ad
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-26 22:12:55 -0300

    feat: changed methods logic for less code replication

--- src/main/java/com/github/vitordalvi/ucloan/services/EquipmentService.java
+++ src/main/java/com/github/vitordalvi/ucloan/services/EquipmentService.java
@@ -3,19 +3,26 @@ package com.github.vitordalvi.ucloan.services;
 import com.github.vitordalvi.ucloan.dto.request.CreateEquipmentRequestDto;
 import com.github.vitordalvi.ucloan.dto.request.PatchEquipmentRequestDto;
 import com.github.vitordalvi.ucloan.dto.response.EquipmentResponseDto;
+import com.github.vitordalvi.ucloan.dto.view.EquipmentView;
+import com.github.vitordalvi.ucloan.entities.ApplicationUser;
 import com.github.vitordalvi.ucloan.entities.Equipment;
 import com.github.vitordalvi.ucloan.entities.EquipmentHistory;
 import com.github.vitordalvi.ucloan.entities.EquipmentModel;
+import com.github.vitordalvi.ucloan.entities.enums.Role;
+import com.github.vitordalvi.ucloan.exceptions.BusinessException;
 import com.github.vitordalvi.ucloan.exceptions.ResourceNotFoundException;
 import com.github.vitordalvi.ucloan.mapper.EquipmentMapper;
 import com.github.vitordalvi.ucloan.repository.EquipmentHistoryRepository;
 import com.github.vitordalvi.ucloan.repository.EquipmentModelRepository;
 import com.github.vitordalvi.ucloan.repository.EquipmentRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.AccessDeniedException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EquipmentService {
@@ -24,40 +31,61 @@ public class EquipmentService {
     private final EquipmentMapper equipmentMapper;
     private final EquipmentModelRepository equipmentModelRepository;
     private final EquipmentHistoryRepository equipmentHistoryRepository;
+    private final LoanService loanService;
 
     public EquipmentService(EquipmentRepository equipmentRepository, EquipmentMapper equipmentMapper,
                             EquipmentModelRepository equipmentModelRepository,
-                            EquipmentHistoryRepository equipmentHistoryRepository) {
+                            EquipmentHistoryRepository equipmentHistoryRepository,
+                            LoanService loanService) {
         this.equipmentRepository = equipmentRepository;
         this.equipmentMapper = equipmentMapper;
         this.equipmentModelRepository = equipmentModelRepository;
         this.equipmentHistoryRepository = equipmentHistoryRepository;
+        this.loanService = loanService;
     }
 
     // Retorna o equipamento específico pelo seu Id
-    public EquipmentResponseDto findById(Long id) {
+    public EquipmentView findById(Long id, ApplicationUser user) {
+        // Se o equipamento não existir, retorna exception
         Equipment equipment = equipmentRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 
+        // Se o usuário for admin
+        if (user.getRole() == Role.ADMIN) {
+            // Retorna o equipment response de admin
+            return equipmentMapper.toDtoAdmin(equipment);
+        }
+
+        // Query (Se o equipamento está alugado, trás o usuário que alugou)
+        Optional<Long> borrowerId = loanService.findBorrowerIdByEquipmentId(id);
+
+        // Se alugador não for nulo, verifica se o id do alugador é o mesmo do usuário
+        if (borrowerId.isPresent() && !borrowerId.get().equals(user.getId())) {
+            throw new BusinessException("You don't have access to see this equipment!");
+        }
+
+        // Retorna o DTO caso passe pelas validações
         return equipmentMapper.toDto(equipment);
     }
 
-    /* FindAll em List
-    public List<EquipmentResponseDto> findAll() {
-        List<Equipment> equipments = equipmentRepository.findAll();
-
-        return equipmentMapper.toDtoList(equipments);
-    }
-    */
-
     // Retorno de todos os equipamentos em página
-    public Page<EquipmentResponseDto> findAll(Pageable pageable) {
-        Page<Equipment> equipments = equipmentRepository.findAll(pageable);
+    @Transactional
+    public Page<EquipmentView> findAll(ApplicationUser user, Pageable pageable) {
+        // Se o usuário for administrador, retorna todos os equipamentos
+        if (user.getRole() == Role.ADMIN) {
+            Page<Equipment> equipments = equipmentRepository.findAll(pageable);
+
+            return equipments.map(equipmentMapper::toDtoAdmin);
+        }
+
+        // Se o cargo for USER, retorna somente os equipamentos que estão disponíveis para empréstimo
+        Page<Equipment> equipments = equipmentRepository.findAllAvailable(pageable);
 
         return equipments.map(equipmentMapper::toDto);
     }
 
     // Cria um equipamento
+    @Transactional
     public EquipmentResponseDto create(CreateEquipmentRequestDto dto) {
         // Validação de se o equipmentModelId que foi passado realmente existe no banco
         EquipmentModel equipmentModel = equipmentModelRepository.findById(dto.equipmentModelId())
@@ -72,6 +100,7 @@ public class EquipmentService {
     }
 
     // Atualiza todos os campos de um equipamento específico
+    @Transactional
     public EquipmentResponseDto update(Long id, CreateEquipmentRequestDto dto) {
         // Procura o modelo de equipamento
         EquipmentModel equipmentModel = equipmentModelRepository.findById(dto.equipmentModelId())
@@ -89,10 +118,18 @@ public class EquipmentService {
     }
 
     // Atualiza campos específicos de um equipamento
-    public EquipmentResponseDto patch(Long id, PatchEquipmentRequestDto dto) {
+    @Transactional
+    public EquipmentView patch(Long id, PatchEquipmentRequestDto dto) {
         // Procura o equipamento
         Equipment equipment = equipmentRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        // Se os campos não foram alterados, retorna o equipamento como dto
+        if (dto.equipmentModelId() == null && dto.physicalStatus() == null && dto.description() == null) {
+            return equipmentMapper.toDtoAdmin(equipment);
+        }
+
+        EquipmentHistory history = new EquipmentHistory();
 
         // Se o equipmentModel foi alterado
         if (dto.equipmentModelId() != null) {
@@ -102,19 +139,29 @@ public class EquipmentService {
 
             // Se achar, seta o novo modelo de equipamento
             equipment.setEquipmentModel(equipmentModel);
+            history.setEquipmentModel(equipmentModel);
+
         }
 
         // Se o estado físico mudou
         if (dto.physicalStatus() != null && dto.physicalStatus() != equipment.getPhysicalStatus()) {
             // Cria um novo histórico de equipamento e coloca o novo estado físico do equipamento
-            EquipmentHistory history = new EquipmentHistory(equipment, dto.physicalStatus(), "Status updated");
-            equipmentHistoryRepository.save(history); // Salva o histórico no banco
+            history.setPhysicalStatus(dto.physicalStatus());
+        }
+
+        // Se descrição mudou
+        if (dto.description() != null) {
+            equipment.setDescription(dto.description()); // Seta a descrição
+            history.setEquipmentDescription(dto.description()); // Seta a descrição no histórico
         }
 
         equipmentMapper.patchEntityFromDto(dto, equipment); // Atualiza os campos do equipamento pelo dto
         equipmentRepository.save(equipment); // Salva alterações no banco
 
-        return equipmentMapper.toDto(equipment); // Retorna o dto do equipamento
+        history.setNotes("Equipment updated!"); // Adicionada a nota do update feito
+        equipmentHistoryRepository.save(history); // Salva o histórico criado
+
+        return equipmentMapper.toDtoAdmin(equipment); // Retorna o dto do equipamento
     }
 
     // Deleta um equipamento específico pelo seu id
