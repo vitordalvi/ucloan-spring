436573071257e308f3c0716f47c03173235afa6e
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-28 22:01:50 -0300

    feat: added validations into fields

--- src/main/java/com/github/vitordalvi/ucloan/dto/request/CreateEquipmentModelRequestDto.java
+++ src/main/java/com/github/vitordalvi/ucloan/dto/request/CreateEquipmentModelRequestDto.java
@@ -1,8 +1,13 @@
 package com.github.vitordalvi.ucloan.dto.request;
 
+import com.github.vitordalvi.ucloan.dto.view.EquipmentModelView;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CreateEquipmentModelRequestDto(
-        @NotBlank @Size(min = 3, max = 120) String name,
-        @NotBlank @Size(min = 3, max = 120) String manufacturer) {}
+        @NotBlank(message = "O campo nome não pode ser nulo")
+        @Size(min = 3, max = 120, message = "O campo nome deve conter entre 3 à 120 caracteres")
+        String name,
+        @NotBlank(message = "O campo fabricante não pode ser nulo")
+        @Size(min = 3, max = 120, message = "O campo fabricante deve conter entre 3 à 120 caracteres")
+        String manufacturer) implements EquipmentModelView {}
--- src/main/java/com/github/vitordalvi/ucloan/dto/request/PatchEquipmentModelRequestDto.java
+++ src/main/java/com/github/vitordalvi/ucloan/dto/request/PatchEquipmentModelRequestDto.java
@@ -1,8 +1,11 @@
 package com.github.vitordalvi.ucloan.dto.request;
 
+import com.github.vitordalvi.ucloan.dto.view.EquipmentModelView;
 import jakarta.validation.constraints.Size;
 
 public record PatchEquipmentModelRequestDto(
-        @Size(min = 3, max = 120) String name,
-        @Size(min = 3, max = 120) String manufacturer
-) {}
+        @Size(min = 3, max = 120, message = "O campo nome deve conter entre 3 à 120 caracteres")
+        String name,
+        @Size(min = 3, max = 120, message = "O campo fabricante deve conter entre 3 à 120 caracteres")
+        String manufacturer
+) implements EquipmentModelView {}
