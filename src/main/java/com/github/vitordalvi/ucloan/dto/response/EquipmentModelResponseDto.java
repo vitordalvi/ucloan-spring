c77ad8c9b62b1eecd652a20c68b53dea4f183337
Vitor Dalvi <vitordalvi@gmail.com>
2026-07-28 22:02:24 -0300

    feat: implement equipment model view into dto

--- src/main/java/com/github/vitordalvi/ucloan/dto/response/EquipmentModelResponseDto.java
+++ src/main/java/com/github/vitordalvi/ucloan/dto/response/EquipmentModelResponseDto.java
@@ -1,3 +1,5 @@
 package com.github.vitordalvi.ucloan.dto.response;
 
-public record EquipmentModelResponseDto(Long id, String name, String manufacturer) {}
+import com.github.vitordalvi.ucloan.dto.view.EquipmentModelView;
+
+public record EquipmentModelResponseDto(Long id, String name, String manufacturer) implements EquipmentModelView {}
