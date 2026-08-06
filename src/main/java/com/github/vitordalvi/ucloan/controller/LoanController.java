529ff45b3e76dc8f399e57f996828f1ef4d1c390
Vitor Dalvi <vitordalvi@gmail.com>
2026-08-05 21:50:04 -0300

    feat: added creation endpoint

--- src/main/java/com/github/vitordalvi/ucloan/controller/LoanController.java
+++ src/main/java/com/github/vitordalvi/ucloan/controller/LoanController.java
@@ -1,5 +1,8 @@
 package com.github.vitordalvi.ucloan.controller;
 
+import com.github.vitordalvi.ucloan.dto.request.CreateLoanRequestDto;
+import com.github.vitordalvi.ucloan.dto.response.LoanAdminResponseDto;
+import com.github.vitordalvi.ucloan.dto.response.LoanResponseDto;
 import com.github.vitordalvi.ucloan.dto.view.LoanView;
 import com.github.vitordalvi.ucloan.entities.ApplicationUser;
 import com.github.vitordalvi.ucloan.services.LoanService;
@@ -9,11 +12,12 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.nio.file.AccessDeniedException;
 
 @RestController
@@ -26,12 +30,9 @@ public class LoanController {
         this.loanService = loanService;
     }
 
-    // 
     @GetMapping("/{id}")
     public ResponseEntity<LoanView> getLoan(@PathVariable Long id,
-                                            Authentication authentication) throws AccessDeniedException {
-        var user = (ApplicationUser) authentication.getPrincipal();
-
+                                            @AuthenticationPrincipal ApplicationUser user) throws AccessDeniedException {
         LoanView response = loanService.findById(id, user);
 
         return ResponseEntity.ok(response);
@@ -46,8 +47,7 @@ public class LoanController {
 
     @GetMapping("/my-loans")
     public ResponseEntity<Page<LoanView>> getMyLoans(@PageableDefault(size = 10) Pageable pageable,
-                                                            Authentication authentication) {
-        var user = (ApplicationUser) authentication.getPrincipal();
+                                                            @AuthenticationPrincipal ApplicationUser user) {
         Page<LoanView> userLoans = loanService.findAllUserLoans(user, pageable);
 
         return ResponseEntity.ok(userLoans);
@@ -56,10 +56,23 @@ public class LoanController {
     @GetMapping("/{id}/history")
     public ResponseEntity<Page<LoanView>> getLoanHistory(@PathVariable Long id,
                                                          @PageableDefault(size = 10) Pageable pageable,
-                                                         Authentication authentication) {
-        var user = (ApplicationUser) authentication.getPrincipal();
+                                                         @AuthenticationPrincipal ApplicationUser user) {
         Page<LoanView> response = loanService.findLoanHistoryById(id, user, pageable);
 
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<LoanResponseDto> createLoan(@RequestBody CreateLoanRequestDto dto,
+                                                      UriComponentsBuilder uriBuilder) {
+
+        LoanResponseDto response = loanService.createLoan(dto);
+
+        URI location = uriBuilder.path("/api/v1/loans/{id}")
+                .buildAndExpand(response.loanId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(response);
     }
 }
