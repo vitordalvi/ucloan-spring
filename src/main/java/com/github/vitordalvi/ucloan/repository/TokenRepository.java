11c707ae9c277a8bd557495f86f7252aa1feaf22
Vitor Dalvi <vitordalvi@gmail.com>
2026-08-21 12:27:06 -0300

    fix: removed logic problem from revoke all user tokens query

--- src/main/java/com/github/vitordalvi/ucloan/repository/TokenRepository.java
+++ src/main/java/com/github/vitordalvi/ucloan/repository/TokenRepository.java
@@ -14,7 +14,7 @@ public interface TokenRepository extends JpaRepository<Token, Long> {
     @Query(value = """
         select t from Token t inner join ApplicationUser u\s
                 on t.applicationUser.id = u.id\s
-                where u.id = :id and (t.expired = false or t.revoked = false)\s
+                where u.id = :id and (t.expired = false AND t.revoked = false)\s
         """)
     List<Token> findAllValidTokenByUser(Long id);
 
