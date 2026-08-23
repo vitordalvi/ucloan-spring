c3e26a88484881da7e3282a97b44dd04f35166ef
Vitor Dalvi <vitordalvi@gmail.com>
2026-08-23 20:44:17 -0300

    fix: removed duplicated method

--- src/main/java/com/github/vitordalvi/ucloan/repository/TokenRepository.java
+++ src/main/java/com/github/vitordalvi/ucloan/repository/TokenRepository.java
@@ -19,5 +19,4 @@ public interface TokenRepository extends JpaRepository<Token, Long> {
     List<Token> findAllValidTokenByUser(Long id);
 
     Optional<Token> findByToken(String token);
-    Optional<Token> findTokenByToken(String token);
 }
