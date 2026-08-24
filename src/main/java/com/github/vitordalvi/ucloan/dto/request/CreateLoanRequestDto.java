d03108c212aa41fa8aa5c311e2436d5f4aac1e65
Vitor Dalvi <vitordalvi@gmail.com>
2026-08-24 11:37:48 -0300

    feat: add dto validations and date formatter

--- src/main/java/com/github/vitordalvi/ucloan/dto/request/CreateLoanRequestDto.java
+++ src/main/java/com/github/vitordalvi/ucloan/dto/request/CreateLoanRequestDto.java
@@ -1,6 +1,7 @@
 package com.github.vitordalvi.ucloan.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -10,6 +11,10 @@ import java.time.LocalDateTime;
 public record CreateLoanRequestDto(
         @NotNull Long borrowerId,
         @NotNull Long equipmentId,
-        @JsonFormat(pattern = "dd/MM/yyyy") LocalDate endDate,
+
+        @JsonFormat(pattern = "dd/MM/yyyy")
+        @FutureOrPresent(message = "End date must be today or in the future")
+        @NotNull(message = "A data final deve ser inserida no padrão: dd/MM/yyyy") LocalDate endDate,
+
         @Size(max = 300) String description
 ) {}
--- src/main/java/com/github/vitordalvi/ucloan/dto/request/ExtendLoanDurationRequestDto.java
+++ src/main/java/com/github/vitordalvi/ucloan/dto/request/ExtendLoanDurationRequestDto.java
@@ -1,8 +1,11 @@
 package com.github.vitordalvi.ucloan.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record ExtendLoanDurationRequestDto(
-        Long loanId,
-        LocalDate extendDuration
+        @NotNull Long loanId,
+        @JsonFormat(pattern = "dd/MM/yyyy") @NotNull LocalDate extendDuration
 ) {}
